d2b4198f7d6477b652b986912c3e2e954eef90ed
mukesh <mukesh.yerra@gmail.com>
2024-07-28 16:45:44 +0530

    Ingestion phase implemented

--- template.py
+++ template.py
@@ -4,10 +4,11 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
+
 project_name = "textSummarizer"
 
 list_of_files = [
-    "./github/workflows/.gitkeep",
+    ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/conponents/__init__.py",
     f"src/{project_name}/utils/__init__.py",
@@ -25,24 +26,26 @@ list_of_files = [
     "Dockerfile",
     "requirements.txt",
     "setup.py",
-    "research/trails.ipynb"
+    "research/trials.ipynb",
 
 ]
+
 
 for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
 
     if filedir != "":
-        os.makedirs(filedir,exist_ok=True)
-        logging.info(f"Creating directory: {filedir} for the file {filename}")
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory:{filedir} for the file {filename}")
 
     
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath,'w') as f:
             pass
             logging.info(f"Creating empty file: {filepath}")
 
-        
+
+    
     else:
         logging.info(f"{filename} is already exists")
